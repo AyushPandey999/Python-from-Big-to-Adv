8e35550f8fbfd608f420ca4db8020250065aaebd
Ayush Pandey <shivamraj2f@gmail.com>
2025-09-16 18:41:19 +0530

    Create what.py

---
+++ Basic/what.py
@@ -0,0 +1,97 @@
+#What is Programming Language ?
+
+"""
+Programing Language is the way to tell the computer what to do.
+"""
+
+# What is Python ?
+
+"""
+Python is an high level object oriented dynamic type interpreted scripting language developed by guido van rossam in 1991.
+"""
+
+# What are the features of python ?
+
+"""
+The features of Python are:
+1) High Level.
+2) Object Oriented.
+3) Interpreted.
+4) Scripting Language.
+5) Platform Independent.
+6) Easy to Learn.
+7) Easy to Use.
+8) Huge set of Library Function.
+9) Support Multiple Programming Paradigms. 
+10) Free and Open Source.
+"""
+
+# What is Use of Python ?
+
+"""
+Uses of Python :
+1) AI/ML.
+2) Data Science.
+3) Data Analysis.
+4) Backend.
+5) Software Development.
+6) Software Testing.
+"""
+
+# What is Difference Between Low-Level , High-Level , Mid-Level Languages ?
+
+"""
+1) Low Level Language : The Language which is easily understandable by the machine and hard by the humans. ex : binary Number.
+2) Mid Level Language : The Language which is partially understandable by the machine and humans. ex : C sharp.
+3) High Level Language : The Language easily understandable by the humans and hard by machines . ex : Python.
+"""
+
+# What is Dynamic Type Language ?
+
+"""
+The Language which decide the variable type at the run time.
+ex :
+    a=10 (int)
+    a=10.5 (float)
+"""
+
+# What is Interpreted Language ?
+
+"""
+The Language which check line by line of code , stop at the error and give the output without checking whole code.
+"""
+
+# What is Scripting Language ?
+
+"""
+The Language which use the Interpreter as a Translator.
+"""
+
+# What is Object - Oriented Language ?
+
+"""
+The Language which supports oops principles , enabling modules , reusable , extensible code through concepts like classes , objects...etc
+
+oops :
+1) Class.
+2) Object.
+3) Inheritance.
+4) Polymorphism.
+5) Encapsulation.
+6) Abstracton.
+"""
+
+# What is Platform Independent Language ?
+
+"""
+The Language which run on any operating system like macos , windows , linux ...etc
+"""
+
+# Support Multiple Programming Paradigms ?
+
+"""
+Support Multiple Programming Paradigms :
+1)Oops (Object Oriented Programming)
+2)Fop (Functional Object Oriented)
+3)Pop (Procedual Object Oriented)
+"""
